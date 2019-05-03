87d4b081a24edd1dcb0b273946100407a8e79658
tk3413 <khan.taimore@gmail.com>
2019-05-03 17:14:34 -0400

    initial commit

---
+++ src/main/java/com/example/tradegecko/TradegeckoApplication.java
@@ -0,0 +1,13 @@
+package com.example.tradegecko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradegeckoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradegeckoApplication.class, args);
+	}
+
+}
